90a9ddfecef0ea0d6a2aa1f403a134c167426d5f
Sergiy Zarubin <szarubin@csumb.edu>
2018-10-26 01:17:04 +0000

    lab 1

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
-<html><body>
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <title>Main PHP website </title>
+        <meta charset="UTF-8" > 
+        
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>   
+            <header>
+            <h1>Wade Wilson</h1>
+            <hr id="nav"/>
+            <nav>
+                <a href="/labs/lab1/index.html">Lab1</a>
+                <a href="about.html">Abut</a>
+                <a href="contact.html">Contact</a>
+            </nav>
+        </header>
+ <main><div>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'This is Fracking Boarinbg';
-
+echo 'Like my new toy?';
 ?>
-</body>
+</div>
+</main>
+    </body>
 </html>
