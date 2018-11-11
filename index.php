088e123c90f04f210e7f68351930acb9d5064a86
Sergiy Zarubin <szarubin@csumb.edu>
2018-11-11 17:24:30 +0000

    Lab4

--- index.php
+++ index.php
@@ -11,31 +11,53 @@ in html
 
 <!-- This is the head -->
 <!-- All styles and javascript go inside the head -->
+<html>
     <head>
-        <title>Main PHP website </title>
-        <meta charset="UTF-8" > 
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <title>CST336 App Page</title>
     </head>
-<!-- closing head -->
+    <body>
+    <div class='container'>
+        <div class='text-center'>
+            
+            <!-- Bootstrap Navagation Bar -->
+            <nav class='navbar navbar-default - navbar-fixed-top'>
+                <div class='container-fluid'>
+                    <div class='navbar-header'>
+                        <a class='navbar-brand' href='#'>Sergiy Zarubin</a>
+                    </div>
+                    <ul class='nav navbar-nav'>
+                        <li><a href='index.php'>Home</a></li>
+                        <li><a data-toggle="collapse" href="#labs" role="button" aria-expanded="false" aria-controls="labs">Labs</a>
+                        <li><a data-toggle="collapse" href="#homework" role="button" aria-expanded="false" aria-controls="homework">Home Work</a>
+                    </ul>
+                    <div class="collapse" id="labs">
+                        <ul class='nav navbar-nav'>
+                            <li><a href="/labs/lab1/index.html">Lab 1</a></li>
+                            <li><a href="/labs/lab2/index.php">Lab 2</a></li>
+                            <li><a href="/labs/lab3/index.php">Lab 3</a></li>
+                            <li><a href="/labs/lab3/index.php">Lab 4</a></li>
+                            <li><a href="/labs/lab3/index.php">Lab 5</a></li>
+                        </ul>
+                    </div>
+                    <div class="collapse" id="homework">
+                        <ul class='nav navbar-nav'>
+                            <li><a href="/HW/HW1/index.html">Homework 1</a></li>
+                            <li><a href="/HW/HW2/index.php">Homework 2</a></li>
+                            <li><a href="/HW/HW3/index.php">Homework 3</a></li>
+                            <li><a href="/HW/HW4/index.php">Homework 4</a></li>
+                            <li><a href="/HW/HW5/index.php">Homework 5</a></li>
+                        </ul>
+                    </div>
 
-    <!-- This is the body -->
-    <!-- This is where we place the content of our website -->
-    <body>   
-            <header>
-            <h1>Wade Wilson</h1>
-            <hr id="nav"/>
-            <nav>
-                <a href="/labs/lab1/index.html">Lab 1</a>
-                <a href="/labs/lab2/index.php">Lab 2</a>
-                <a href="/labs/lab3/index.php">Lab 3</a>
-                <a href="/labs/lab3/index.php">Lab 4</a>
-                <a href="/labs/lab3/index.php">Lab 5</a>
-                <a href="/HW/HW1/index.html">Homework 1 </a>   
-                <a href="/HW/HW2/index.php">Homework 2</a>   
-                <a href="/HW/HW3/index.php">Homework 3</a>   
-                <a href="about.html">Abut</a>
-                <a href="contact.html">Contact</a>
+                </div>
             </nav>
+            <br /> <br /> <br />
         </header>
  <main><div>
 <?php
