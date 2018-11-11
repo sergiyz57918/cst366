44aaac2049775524337201881a9fdb4b11dfe0e2
Sergiy Zarubin <szarubin@csumb.edu>
2018-11-11 05:45:06 +0000

    Homework 3

--- HW/HW3/inc/functions.php
+++ HW/HW3/inc/functions.php
@@ -70,33 +70,35 @@ function Mod($a, $b)
 function vigenereCipher($input, $key, $encipher)
 {
 	$keyLen = strlen($key);
+    if($keyLen>0){
+    	for ($i = 0; $i < $keyLen; ++$i)
+    		if (!ctype_alpha($key[$i]))
+    			return ""; // Error
+    
+    	$output = "";
+    	$nonAlphaCharCount = 0;
+    	$inputLen = strlen($input);
 
-	for ($i = 0; $i < $keyLen; ++$i)
-		if (!ctype_alpha($key[$i]))
-			return ""; // Error
-
-	$output = "";
-	$nonAlphaCharCount = 0;
-	$inputLen = strlen($input);
-
-	for ($i = 0; $i < $inputLen; ++$i)
-	{
-		if (ctype_alpha($input[$i]))
-		{
-			$cIsUpper = ctype_upper($input[$i]);
-			$offset = ord($cIsUpper ? 'A' : 'a');
-			$keyIndex = ($i - $nonAlphaCharCount) % $keyLen;
-			$k = ord($cIsUpper ? strtoupper($key[$keyIndex]) : strtolower($key[$keyIndex])) - $offset;
-			$k = $encipher ? $k : -$k;
-			$ch = chr((Mod(((ord($input[$i]) + $k) - $offset), 26)) + $offset);
-			$output .= $ch;
-		}
-		else
-		{
-			$output .= $input[$i];
-			++$nonAlphaCharCount;
-		}
-	}
+    	for ($i = 0; $i < $inputLen; ++$i)
+    	{
+    		if (ctype_alpha($input[$i]))
+    		{
+    			$cIsUpper = ctype_upper($input[$i]);
+    			$offset = ord($cIsUpper ? 'A' : 'a');
+    			$keyIndex = ($i - $nonAlphaCharCount) % $keyLen;
+    			$k = ord($cIsUpper ? strtoupper($key[$keyIndex]) : strtolower($key[$keyIndex])) - $offset;
+    			$k = $encipher ? $k : -$k;
+    			$ch = chr((Mod(((ord($input[$i]) + $k) - $offset), 26)) + $offset);
+    			$output .= $ch;
+    		}
+    		else
+    		{
+    			$output .= $input[$i];
+    			++$nonAlphaCharCount;
+    		}
+    	}
+            
+    }
 
 	return $output;
 }
--- HW/HW3/index.php
+++ HW/HW3/index.php
@@ -1,83 +1,121 @@
 <?php
-session_start(); 
+session_start();
+
 $message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin placerat erat non lectus finibus, eget pulvinar nibh placerat. Aliquam erat volutpat. Pellentesque lacinia porta risus vel iaculis. Morbi id porta erat. Cras suscipit, lacus eget sollicitudin dapibus, tellus nulla laoreet massa, sed fringilla metus elit nec lorem. Sed finibus faucibus nibh et imperdiet. Suspendisse ac ante id turpis cursus fringilla. Aenean tempus tristique enim ut pharetra. Nullam vel aliquet arcu, sed sagittis purus. Suspendisse potenti. Pellentesque vitae elit sagittis, pellentesque orci ac, gravida mauris. Cras viverra nibh vitae condimentum mollis. Morbi in lorem in diam vulputate congue eget quis mauris. Praesent accumsan finibus nisl, quis varius nibh. Pellentesque vulputate quis eros vitae tempus."; 
 $cipherAlphabet = "yhkqgvxfoluapwmtzecjdbsnri";
 $key = "cipher"; 
+if(isset($_POST['cipher'])){
+    include 'inc/functions.php'; 
+    $cipher = $_POST['cipher']; 
+    if($cipher=="sub" ){
+        if(isset($_POST['cipherAlphabet']) and strlen($_POST['cipherAlphabet'])==26){
+            $isOK = subEncipher($message,$_POST['cipherAlphabet'],$cipherText);
+       
+        }else {
+                  $cipherText = "ERROR You need to enter scrambled alphabet of exact 26 leters no repeats";
+                  $isError = true;
+            }
+    } else if ($cipher=="shift"){
+            if(isset($_POST['shiftby']) and is_numeric($_POST['shiftby'])){
+                $shiftBy=$_POST['shiftby']; 
+                $cipherText = shiftEncipher($message,$_POST['shiftby']);
+            }else {
+                  $cipherText = "ERROR You need to enter number from 1 to 26";
+                  $isError = true;
+            }
+        } else if ($cipher=="vigenere"){
+            if(isset($_POST['keyStr']) and strlen($_POST['keyStr'])>0){
+                $keyStr = $_POST['keyStr'];
+                $cipherText = vigenereEncipher($message,$_POST['keyStr']);
+            } else{
+              $cipherText = "ERROR You need to enter a key for the cipher"; 
+              $isError = true; 
+            }
+        } else {
+            $cipherText = "ERROR Well this sucks</div>";
+            $isError = true; 
+        
+        }
+        if (!$isError){
+            $display = "Encripted Message:<br/>
+<textarea name='encriptedTXT' rows='15' cols='60'>".$cipherText."</textarea>";
+        } else {
+            $display = "<div class='error'>".$cipherText."</div>";
+            
+        }
+    $display = $display ."<div>Chipher:".$_POST['cipher']."</div>";
+    $display = $display ."<div>Alphabet:".$_POST['cipherAlphabet']."</div>";
+    $display = $display ."<div>ShiftBy:".$_POST['shiftby']."</div>";
+    $display = $display ."<div>KeyString:".$_POST['keyStr']."</div>";
+    session_destroy();
+}else {
+    $display = "<div class='error'>You will need to select a cipher</div>";
+}
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <title>Lets Encript</title>
-        <style type="text/css">
-            .hide {
-                      display: none;
-                    }
-        </style>
+
         <script type="text/javascript">
         $(document).ready(function(){
             $('input[type="radio"]').click(function(){
                 var inputValue = $(this).attr("value");
                 var targetBox = $("." + inputValue);
-                $(".hide").not(targetBox).hide();
+                $(".box").not(targetBox).hide();
                 $(targetBox).show();
             });
         });
         </script>
+         <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
         <div class='container'>
             <div class='text-center'>
-                
-                <!-- Bootstrap Navagation Bar -->
-                <nav class='navbar navbar-default - navbar-fixed-top'>
-                    <div class='container-fluid'>
-                        <div class='navbar-header'>
-                            <a class='navbar-brand' href='#'>Cryptography</a>
-                        </div>
-                        <ul class='nav navbar-nav'>
-                            <li><a href='index.php'>Home</a></li>
-                        </ul>
-                    </div>
-                </nav>
+                <h2>Let's try some encription algorythms</h2>
+                            <a href='index.php' class="myButton">Home</a>
+  
                 <br /> <br /> <br />
                 <form method="post">
-                      Message to encript:<br>
-                      <textarea name="message" rows="15" cols="60">
+                    <div class="form-group">
+                      Message to encript:<br/>
+                      <textarea placeholder="Enter message to encript..." name="message" rows="15" cols="60">
                           <?php echo $message; ?>
                       </textarea>
+                    </div>
                       <br/>
                       <br/>
-                      <div><ol>
-                        <ul><input type="radio" name="cipher" value="sub"/><label for="sub">Substitution Cipher</label></ul>
-                        <ul><input type="radio" name="cipher" value="shift"/><label for="shift">Shift Cipher Cipher</label></ul>
-                        <ul><input type="radio" name="cipher" value="vigenere"/><label for="shift">Vigenere Cipher</label> </ul>
-                        </ol>
-                      </div>
-                      <div class="sub hide">
-                                            <textarea name="message" rows="1" cols="27">
-                                              <?php echo $cipherAlphabet; ?>
-                                            </textarea>
-                      </div>
-                      <div class="shift hide">
-                                            <input type="number" name="shiftby"/><br/>
-                      </div>
-                      <div class="vigenere hide">
-                                            <input type="text" name="key" value="<?php echo $key; ?>"/>
-                      </div>
+                    <div>
+                        <label for="sub"><input  type="radio" name="cipher" value="sub" <?php echo ($cipher=="sub")?"checked":"" ?>/>Substitution Cipher</label>
+                    </div>
+                    <div>
+                        <label for="shift"><input  type="radio" name="cipher" value="shift" <?php echo ($cipher=="shift")?"checked":"" ?>/>Shift Cipher Cipher</label>
+                    </div>
+                    <div>
+                        <label for="shift"><input type="radio" name="cipher" value="vigenere" <?php echo ($cipher=="vigenere")?"checked":""?>/>Vigenere Cipher</label>
+                    </div>    
+
+                    <div class="sub box" <?php echo ($cipher=="sub")?"style='display: block;'":"" ?>>
+                          <input  type="text" placeholder="New alphabet..." name="cipherAlphabet" <?php echo isset($cipherAlphabet)?"value='$cipherAlphabet'":""; ?>/><br/>
+                                              
+                    </div>
+                    <div class="shift box" <?php echo ($cipher=="shift")?"style='display: block;'":"" ?>>
+                                            <input  type="number" name="shiftby" <?php echo isset($shiftBy)?"value='$shiftBy'":""; ?> placeholder="Enter a number..."/><br/>
+                    </div>
+                    <div class="vigenere box" <?php echo ($cipher=="vigenere")?"style='display: block;'":"" ?>>
+                                            <input  type="text" name="keyStr" <?php echo isset($keyStr)?"value='$keyStr'":""; ?> placeholder="Enter a key..."/><br/>
+                    </div>
 
                           
                       <div>
-                      <input type="submit">
-                      <input type="reset">
+                            <button class="button" type="submit">Encript Message</button>
                       </div>
                 </form>
-
+                <hr/>
+                <?php echo $display ?>
 
             </div>
         </div>
